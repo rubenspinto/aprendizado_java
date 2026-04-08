056fe2412fb08d1d8b057d768e0a7a9fa8c43e39
rubenspinto <rubenssi825@gmail.com.com>
2026-04-08 12:56:54 -0300

    fix: improve comments and documentation in Principal.java

--- src/condicionais/labs/Principal.java
+++ src/condicionais/labs/Principal.java
@@ -1,14 +1,21 @@
 package condicionais.labs;
 
+/**
+ * Faça um programa que leia o nome e a idade de uma pessoa e imprima uma mensagem dizendo se ela é maior ou menor de idade.
+ */
+
 public class Principal {
 
     public static void main(String[] args) {
 
+        // Criando um objeto Scanner para ler a entrada do usuário
         var entrada = new java.util.Scanner(System.in);
 
+        // Solicitando ao usuário que digite seu nome e idade
         System.out.println("Digite seu nome: ");
         var nome = entrada.nextLine();
 
+        // Solicitando ao usuário que digite sua idade
         System.out.println("Digite sua idade: ");
         var idade = entrada.nextInt();
 
